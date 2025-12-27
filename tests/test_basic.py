2ff2a6d9b89f039dad82208f1da4ce1f9893f683
Yoshiyuki <64542671+yHatake223-maker@users.noreply.github.com>
2025-12-27 14:06:52 +0000

    Break CI intentionally

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,5 +2,6 @@ from everyday_helloworld.main import hello
 
 
 def test_hello():
-    assert hello() == "Hello, World!"
+    #assert hello() == "Hello, World!"
+    assert hello("World") == "Hello, CI!"
     assert hello("Python") == "Hello, Python!"
